8a2b52619e0deab31601d07db5c2d3def4a341f1
Prof. Márcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2020-03-12 20:16:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# atividade-fake
+
+Link do _classroom_ <https://classroom.github.com/a/za6mMIPG>
+
+Suba um arquivo txt com seu nome.
---
+++ src/main/java/aps/Main.java
@@ -0,0 +1,10 @@
+package aps;
+
+public class Main {
+	public static void main(String[] args) {
+
+		CPF cpf = new CPF("11122233344");
+		System.out.println(cpf.toString().equals("111.222.333-44"));
+
+	}
+}
